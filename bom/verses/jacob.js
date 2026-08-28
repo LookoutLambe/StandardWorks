c8de8a852452403f3acc66edaa05c35b4296e4d3
Christopher Lambe <chrislambe03@gmail.com>
2026-08-28 15:25:25 -0600

    Mormon Dual omission pass: conform to standard, sof pasuq; Shaddai ruling (Jacob 2:5, Mormon 9:26)

--- bom/verses/jacob.js
+++ bom/verses/jacob.js
@@ -278,7 +278,7 @@ var jc_ch2Verses = [
     ["בּוֹרֵא","Creator"],
     ["שָׁמַיִם","heaven"],
     ["וָאָרֶץ","and earth"],
-    ["הַכֹּל־יָכוֹל","the all powerful"],
+    ["שַׁדַּי","the all-powerful"],
     ["אוּכַל","I am able"],
     ["לְהַגִּיד","to declare"],
     ["לָכֶם","unto you"],
